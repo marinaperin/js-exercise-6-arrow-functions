4f4e63e8a794c348ccdab18fc0f8adc830e6530d
Marina Perin <maryfrancisco@live.com>
2023-11-06 21:10:45 +0100

    feat | completed exercises

--- main.js
+++ main.js
@@ -24,14 +24,45 @@ window.addEventListener('load', ()=>{
 // Exercise 4
 
 window.addEventListener('load', ()=>{
+    const ul = document.getElementById('ul');
     const lastLi = document.createElement('li');
     lastLi.innerText = 'item 4';
-    const ul = document.getElementById('ul');
     ul.appendChild(lastLi);
     const item5 = document.createElement('li');
     item5.innerText = 'item 5';
     ul.insertBefore(item5, ul.children[0]);
-})
+});
 
 // Exercise 5
 
+window.addEventListener('load', ()=>{
+    const divBlu = document.getElementById('blu-div');
+    divBlu.addEventListener('click', function(){
+        if(this.classList.contains('blu')){
+            this.classList.add('rosso');
+            this.classList.remove('blu');
+        }
+    })
+})
+
+// Exercise 6
+
+window.addEventListener('load', ()=>{
+    const button = document.getElementById('button');
+    button.setAttribute('disabled', null);
+});
+
+// Exercise 7
+
+window.addEventListener('load', ()=>{
+    const changingDiv = document.getElementById('ex-7');
+    changingDiv.addEventListener('mouseover', function(){
+        this.style.backgroundColor = 'powderblue';
+    })
+    changingDiv.addEventListener('mouseout', function(){
+        this.style.backgroundColor = 'transparent';
+    })
+    changingDiv.addEventListener('click', function(){
+        console.log('You clicked me!');
+    })
+});
