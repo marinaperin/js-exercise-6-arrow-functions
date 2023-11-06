efba1027ef6151f7a629db0f9503dea3869caddd
Marina Perin <maryfrancisco@live.com>
2023-11-06 15:33:48 +0100

    feat | exercise 2

--- main.js
+++ main.js
@@ -3,3 +3,12 @@
 const subtraction = (num1, num2) => num1 - num2;
 
 // Exercise 2
+
+const student = {
+    name: 'Brian',
+    age: 16,
+    personalInformation: function(){
+        console.log(this.name, this.age);
+    },
+    aging: passingTime = (years) => student.age += years,
+}
